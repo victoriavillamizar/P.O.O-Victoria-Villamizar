3bdfeb5a7e297bf5dd80f390cc6da9676d76a9b4
Villamizar Victoria <villamizarvictoria799@gmail.com>
2026-04-30 12:47:28 -0300

    Add files via upload

---
+++ VillamizarVictoria-act8/ejercicioN1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioN1
+{
+    class cargarNelementos
+    {
+        /*1. Cargar un vector de n elementos. imprimir el menor y un mensaje si se repite dentro
+del vector.*/
+        private int[] vector;
+
+        public void cargarDatos()
+        {
+            vector = new int[10];
+
+            for (int m = 0; m < 10; m++)
+            {
+                Console.WriteLine("ingrese elemento");
+                string linea = Console.ReadLine();
+                vector[m] = int.Parse(linea);
+
+            }
+        }
+        public void imprimirMenor()
+        {
+            int min = vector[0];
+            int cont = 0;
+            for (int m = 0; m < 10; m++)
+            {
+                if (vector[m] < min)
+                {
+                    min = vector[m];
+                }
+                else if (min == vector[m])
+                {
+                    cont++;
+                }
+            }
+            if (cont == vector.Length)
+            {
+                Console.WriteLine("el numero minimo se repite");
+            }
+            else
+            {
+                Console.WriteLine("el numero mas minimo es : " + min);
+            }
+        }
+
+                static void Main(string[] args)
+                {
+                    cargarNelementos type = new cargarNelementos();
+                    type.cargarDatos();
+                    type.imprimirMenor();
+
+                }
+            }
+        }
+    
---
+++ VillamizarVictoria-act8/ejercicioN2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioN2
+{
+    class paises
+    {
+        /*2. Definir un vector donde almacenar los nombres de 5 países. Confeccionar el
+algoritmo de ordenamiento alfabético.*/
+        private string[] nombrePaises;
+
+        public void algoritmo()
+        {
+            string letra;
+            nombrePaises = new string [] { "Venezuela","Chile", "Argentina", "Japon", "Peru" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int m = 0; m < nombrePaises.Length - 1 - i; m++)
+                {
+                    if (nombrePaises[m].CompareTo(nombrePaises[m + 1]) > 0)
+                    {
+                        letra = nombrePaises[m];
+                        nombrePaises[m] = nombrePaises[m + 1];
+                        nombrePaises[m + 1] = letra;
+                    }
+                }
+            }
+            for (int i = 0; i < nombrePaises.Length; i++)
+            { 
+                Console.WriteLine(nombrePaises[i]);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            paises paises = new paises();
+            paises.algoritmo();
+            Console.ReadKey();
+
+        }
+    }
+}
