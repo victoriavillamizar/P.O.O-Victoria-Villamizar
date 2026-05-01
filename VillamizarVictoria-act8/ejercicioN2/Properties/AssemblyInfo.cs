e1df59902dcc2e3db41f19d9523061a98978a10d
Villamizar Victoria <villamizarvictoria799@gmail.com>
2026-05-01 13:54:00 -0300

    Add files via upload

---
+++ VillamizarVictoria-act8/ejercicioN2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ejercicioN2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Escuela Confederacion Suiza")]
+[assembly: AssemblyProduct("ejercicioN2")]
+[assembly: AssemblyCopyright("Copyright © Escuela Confederacion Suiza 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("568ab5e2-d3b1-4914-a7ac-8cddefd25db5")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VillamizarVictoria-act8/ejercicioN4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioN4
+{
+    class nombredepaises
+    {
+       /*4. Cargar en un vector los nombres de 5 países y en otro vector paralelo la cantidad de
+        habitantes de este.Ordenar alfabéticamente e imprimir los resultados.Por último
+        ordenar con respecto a la cantidad de habitantes (de mayor a menor) e imprimir
+        nuevamente.*/
+
+        private string[] paisesNombre;
+        private string[] paisesOrden;
+        private int[] habitantes;
+
+        public void cargaValores()
+        {
+            paisesNombre = new string[5];
+            habitantes = new int[5];
+
+            Console.WriteLine("ingrese el nombre de 5 paises");
+            for (int i = 0; i < paisesNombre.Length; i++)
+            {
+                Console.Write("");
+                paisesNombre[i] = Console.ReadLine(); ;
+            }
+            string linea;
+
+            for (int i = 0; i < paisesNombre.Length; i++)
+            {
+                Console.WriteLine("ingrese los habitantes de " + paisesNombre[i]);
+                Console.Write("");
+                linea = Console.ReadLine();
+                habitantes[i] = int.Parse(linea);
+            }
+        }
+
+        public void ordenarAlfabeticamente()
+        {
+            paisesOrden = new string[5];
+            string var;
+
+            // copiar nombres
+            for (int i = 0; i < paisesNombre.Length; i++)
+            {
+                paisesOrden[i] = paisesNombre[i];
+            }
+
+            // ordenar
+            for (int i = 0; i < paisesOrden.Length; i++)
+            {
+                for (int m = 0; m < paisesOrden.Length - 1 - i; m++)
+                {
+                    if (paisesOrden[m].CompareTo(paisesOrden[m + 1]) > 0)
+                    {
+                        var = paisesOrden[m];
+                        paisesOrden[m] = paisesOrden[m + 1];
+                        paisesOrden[m + 1] = var;
+                    }
+                }
+            }
+
+            Console.WriteLine("Paises ordenados alfabeticamente:");
+
+            for (int i = 0; i < paisesOrden.Length; i++)
+            {
+                Console.WriteLine(paisesOrden[i]);
+            }
+        }
+
+        public void ordenarHabitantesMYMN()
+        {
+            int var1;
+            string var2;
+            for (int i = 0; i < paisesNombre.Length; i++)
+            {
+                for (int m = 0; m < habitantes.Length - 1 - i; m++)
+                {
+                    if (habitantes[m] < habitantes[m + 1])
+                    {
+                        var1 = habitantes[m];
+                        habitantes[m] = habitantes[m + 1];
+                        habitantes[m + 1] = var1;
+                        var2 = paisesNombre[m];
+                        paisesNombre[m] = paisesNombre[m + 1];  
+                        paisesNombre[m + 1] = var2;
+                    }
+                }
+            }
+
+            Console.WriteLine("paises ordenados por habitantes");
+            for (int i = 0; i < habitantes.Length; i++)
+            {
+                Console.WriteLine(paisesNombre[i] + ": " + habitantes[i]);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            nombredepaises paises = new nombredepaises();
+            paises.cargaValores();
+            paises.ordenarAlfabeticamente();
+            paises.ordenarHabitantesMYMN();
+            Console.ReadKey();
+        }
+    }
+}
