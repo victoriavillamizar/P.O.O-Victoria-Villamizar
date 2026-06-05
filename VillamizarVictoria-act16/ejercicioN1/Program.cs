d8d237df24166eb4d235bcbb1c8c231be71b1664
Villamizar Victoria <villamizarvictoria799@gmail.com>
2026-06-05 12:00:52 -0300

    Add files via upload

---
+++ VillamizarVictoria-act16/ejercicioN1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioN1
+{
+   public class Persona{
+
+        /*1. Confeccionar una clase Persona que tenga como atributos el nombre y la
+        edad (definir las propiedades para poder acceder a dichos atributos). Definir
+        un método para imprimirlos. 
+        Plantear una segunda clase Empleado que
+        herede de la clase Persona. Añadir un atributo sueldo ( y su propiedad) y el
+        método para imprimir su sueldo. Definir un objeto de la clase Persona y
+        llamar a sus métodos y propiedades. También crear un objeto de la clase
+        Empleado y llamar a sus métodos y propiedades.*/
+
+        protected string nombre;
+        protected int edad;
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+            get
+            {
+                return edad;
+            }
+        }
+        public void imprimir()
+        {
+            Console.WriteLine(nombre + " tiene " + edad + " años");
+
+        }
+        public class Empleado : Persona
+        {
+            private int sueldo;
+
+            public int Sueldo
+            {
+                set
+                {
+                    sueldo = value;
+                }
+                get
+                {
+                    return sueldo;
+                }
+            }
+            public void imprimirsueldo()
+            {
+                Console.WriteLine(nombre +" tiene " + edad + " años" + " y tiene un sueldo de: " + sueldo);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "maximo";
+            persona.Edad = 16;
+            persona.imprimir();
+
+            Empleado empleado = new Empleado();
+            empleado.Nombre = "victoria";
+            empleado.Edad = 17;
+            empleado.Sueldo = 6767;
+            empleado.imprimirsueldo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VillamizarVictoria-act16/ejercicioN3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioN3
+{
+    class vehiculo
+    {
+        /*3. Crear una clase base Vehículo que contenga atributos marca y
+        velocidadMaxima.
+        Definir dos clases derivadas: Auto y Moto, que hereden de Vehículo. Cada
+        una debe tener un constructor que reciba los valores de los atributos base
+        mediante la palabra clave base, y un atributo propio (cantidadPuertas en
+        Auto, cilindrada en Moto).
+        Crear un objeto de cada clase y mostrar todos sus datos por consola.*/
+        protected string marca;
+        protected double velocidadMaxima;
+         
+
+        public string Auto
+        {
+            set
+            {
+                marca = value;
+            }
+            get
+            {
+                return marca;
+            }
+        }
+        public double VelocidadMaxima
+        {
+            set
+            {
+                velocidadMaxima = value;
+            }
+            get
+            {
+                return velocidadMaxima;
+            }
+        }
+
+        public class auto : vehiculo
+        {
+            private int cantidadPuertas;
+
+            public int CantidadPuertas
+            {
+                set
+                {
+                    cantidadPuertas = value;
+                }
+                get
+                {
+                    return cantidadPuertas;
+                }
+            }
+            public auto(string marca, double velocidadMaxima) : base(marca)
+            {
+                this.CantidadPuertas = puertas;
+            }
+        }
+        public class moto : vehiculo
+        {
+            private int cilindradaMoto;
+
+            public int CilindradaMoto
+            {
+                set
+                {
+                    cilindradaMoto = value;
+                }
+                get
+                {
+                    return cilindradaMoto;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
